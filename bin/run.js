0273c41ed422ef7456a6e7f159252f98b1fa0e3a
donkeykong91 <darylnator@gmail.com>
2018-07-21 05:56:04 -0700

    Starting point

---
+++ bin/run.js
@@ -0,0 +1,39 @@
+"strict mode"
+
+
+const slackClient = require("../server/slackClient");
+
+const service = require("../server/service");
+
+const http = require("http");
+
+const server = http.createServer(service);
+
+
+const witToken = process.env.WIT_TOKEN;
+
+const witClient = require("../server/witClient")(witToken);
+
+
+const slackToken = process.env.SLACK_TOKEN;
+
+const slackLogLevel = 'info';
+
+
+const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
+
+rtm.start();
+
+
+slackClient.addAuthenticatedHandler(rtm, function () {
+
+    server.listen(3000);
+
+});
+
+
+server.on("listening", function() {
+
+    console.log(`IRIS is listening on ${server.address().port} in ${service.get("env")} mode.`);
+
+});
