4168a465b0c8c1981424fce9b9b54b722953ac57
donkeykong91 <darylnator@gmail.com>
2018-07-21 23:28:55 -0700

    Added route called service and it passes a variable called location

--- server/service.js
+++ server/service.js
@@ -6,4 +6,11 @@ const express = require("express");
 const service = express();
 
 
+service.get("/service/:location", function (req, res, next) {
+
+    res.json({result: req.params.location});
+
+});
+
+
 module.exports = service;
