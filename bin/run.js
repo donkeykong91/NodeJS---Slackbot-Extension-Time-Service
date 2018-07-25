163f977b43b21b2cc6c26a434307da82cbc5d27b
donkeykong91 <darylnator@gmail.com>
2018-07-25 04:18:14 -0700

    Added function to listen whether main service is running and logging out what ip main service is on and what port it's using

--- bin/run.js
+++ bin/run.js
@@ -1,6 +1,8 @@
 "strict mode"
 
 
+const superagentRequest = require("superagent");
+
 const service = require("../server/service");
 
 const http = require("http");
@@ -8,11 +10,43 @@ const http = require("http");
 
 const server = http.createServer(service);
 
-server.listen(3010);
+server.listen();
 
 
 server.on("listening", function() {
 
+
     console.log(`IRIS-Time is listening on ${server.address().port} in ${service.get("env")} mode.`);
+
+
+    var asyncAnnounce = async function () {
+
+
+        try {
+
+            var respond = await superagentRequest.put(`http://127.0.0.1:3000/service/time/${server.address().port}`);
+
+
+            console.log(respond.body);
+
+
+        } catch (error) {
+            
+
+            console.log(error);
+
+            console.log("Error connecting to Iris");
+
+            return
+
+        }
+
+    }
+
+
+    asyncAnnounce();
+
+    setInterval(asyncAnnounce, 15*1000);
+
 
 });
